3502a832a632ea4d0cb7e22f4940d30cd9eceaaf
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-06 18:59:45 +0530

    fix: creating using documents

--- src/js/main.js
+++ src/js/main.js
@@ -92,3 +92,71 @@ validateForm.onSuccess(() => {
     formEl.reset();
 });
 
+
+function getAllCourierDatas() {
+    // Get all stored corier datas which are avaliable in localStorage
+    const courierData = localStorage.getItem(localStorageKey)
+
+    const courierDataArr = JSON.parse(courierData)
+
+    // Write those values into the table UI
+    const tableEL = document.getElementById("courierDataTable")
+
+    const fragment = document.createDocumentFragment()
+
+    if (courierDataArr) {
+
+        const courierCardEl = document.querySelector("#courierCard");
+        courierCardEl.classList.remove("hidden");
+
+        const finalData = courierDataArr.map((courierData) => {
+
+            const trEl = document.createElement("tr");
+            const tdCoustomerEL = document.createElement("td");
+            const tdEl = document.createElement("td");
+            const td2El = document.createElement("td");
+            const td3El = document.createElement("td");
+            const td4El = document.createElement("td");
+            const td5El = document.createElement("td");
+            const deleteBtnEl = document.createElement("button")
+
+            trEl.classList.add("px-2", "py-1", "border");
+
+            // tdCoustomerEL.classList.add("px-2", "py-1", "border");
+            // tdCoustomerEL.textContent = index + 1;
+
+            tdEl.classList.add("px-2", "py-1", "border");
+            tdEl.textContent = courierData.name
+
+            td2El.classList.add("px-2", "py-1", "border");
+            td2El.textContent = courierData.number
+
+            td3El.classList.add("px-2", "py-1", "border");
+            td3El.textContent = courierData["pickup-date"]
+
+            td4El.classList.add("px-2", "py-1", "border");
+            td4El.textContent = courierData["pickup-area"]
+
+            deleteBtnEl.className = "px-2 py-1 rounded bg-red-500 hover:bg-red-600 text-white text-sm";
+            deleteBtnEl.textContent = "Delete";
+
+            td5El.classList.add("px-2", "py-1", "border");
+            td5El.append(deleteBtnEl);
+
+
+            trEl.append(tdEl, td2El, td3El, td4El, td5El)
+            fragment.append(trEl)
+        });
+        tableEL.append(fragment);
+
+    } else {
+        console.log("No value available on localStorage");
+    };
+
+
+
+    // display the UI with those datas
+
+
+}
+getAllCourierDatas()
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#312e81"
+      },
+    },
   },
   plugins: [],
 }
